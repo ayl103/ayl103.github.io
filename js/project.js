4c85f9e175a6d1dbb73eed8f560fb88e4052fea5
ayl103 <38549562+ayl103@users.noreply.github.com>
2018-04-30 23:21:13 -0400

    Update project.js

--- js/project.js
+++ js/project.js
@@ -221,7 +221,169 @@ function renderSingleProjectPage(index, data){
 
         container.find('h4').text(item.project.blurb);
         container.find('p').text(item.project.description);
-      }else if(item.id == 65 && item.id == index) {
+      } else if(item.id == 26 && item.id == index) {
+        //erica kermani
+        page.find('.page-name').text(item.name);
+        page.find('.page-image').find('img').attr('src',"portraits/"+item['profile-pic']);
+        if(item.social['portfolio'] != "") {
+          page.find('.portfolio').text("Portflio") ;
+          page.find('.portfolio').attr('href', item.social['portfolio']) ;
+          page.find('#port1').append("&nbsp;&nbsp;|&nbsp;&nbsp;") ;
+          portTrue = true ;
+        }
+        if(item.social['facebook'] != "") {
+          page.find('.fb').text("Facebook") ;
+          page.find('.fb').attr('href', item.social['facebook']) ;
+          page.find('#fb1').append("&nbsp;&nbsp;|&nbsp;&nbsp;") ;
+          fbTrue = true ;
+        }
+        if(item.social['instagram'] != "") {
+          page.find('.insta').text("Instagram") ;
+          page.find('.insta').attr('href', item.social['instagram']) ;
+          page.find('#insta1').append("&nbsp;&nbsp;|&nbsp;&nbsp;") ;
+          instaTrue = true ;
+        }
+        if(item.social['linkedin'] != "") {
+          page.find('.linkedin').text("Linkedin") ;
+          page.find('.linkedin').attr('href', item.social['linkedin']) ;
+          page.find('#linkedin1').append("&nbsp;&nbsp;|&nbsp;&nbsp;") ;
+          linkTrue = true ;
+        }
+        if(item.social['twitter'] != "") {
+          page.find('.twitter').text("Twitter") ;
+          page.find('.twitter').attr('href', item.social['twitter']) ;
+          page.find('#twitter1').append("&nbsp;&nbsp;|&nbsp;&nbsp;") ;
+          twitterTrue = true ;
+        }
+        if(item.social.hasOwnProperty('github') && item.social['github'] != "") {
+          page.find('.github').text("Github") ;
+          page.find('.github').attr('href', item.social['github']) ;
+          page.find('#github1').append("&nbsp;&nbsp;|&nbsp;&nbsp;") ;
+          githubTrue = true ;
+        }
+        if(item.social.hasOwnProperty('tumblr') && item.social['tumblr'] != "") {
+          page.find('.tumblr').text("Tumblr") ;
+          page.find('.tumblr').attr('href', item.social['tumblr']) ;
+          page.find('#tumblr1').append("&nbsp;&nbsp;|&nbsp;&nbsp;") ;
+          tumblrTrue = true ;
+        }
+        if(item.social['other'] != "") {
+          page.find('.other').text(item.social['other']) ;
+          if(item.social['other'][0] == "h") {
+            page.find('.other').attr('href', item.social['other']) ;
+          }
+        } else {
+            if(tumblrTrue === true) {
+              page.find('.tumblr').text("Tumblr") ;
+              page.find('#tumblr1').text("");
+            } else if (githubTrue === true) {
+              page.find('.github').text("Github") ;
+              page.find('#github1').text("");
+            } else if(twitterTrue === true) {
+              page.find('.twitter').text("Twitter") ;
+              page.find('#twitter1').text("") ;
+            } else if (linkTrue === true) {
+              page.find('.linkedin').text("Linkedin") ;
+              page.find('#linkedin1').text("") ;
+            } else if (instaTrue === true) {
+              page.find('.insta').text("Instagram") ;
+              page.find('#insta1').text("") ;
+            } else if(fbTrue === true) {
+              page.find('.fb').text("Facebook") ;
+              page.find('#fb1').text("") ;
+            } else if (portTrue === true) {
+              page.find('.portfolio').text("Portfolio") ;
+              page.find('#port1').text("") ;
+            }
+        }
+        for(var i = 0 ; i < item.aboutme.length ; i++) {
+          page.find('.page-desc').append(item.aboutme[i]+ "<br>" + "<br>") ;
+        }
+
+        if(item.project['website'] != "") {
+          page.find('.websiteLink').text("Project Website") ;
+          page.find('.websiteLink').attr('href', item.project['website']) ;
+          instaTrue = true ;
+        }
+
+
+        page.find('.project-name').text(item.project.title);
+
+        var newText = document.createElement('h5');
+        newText.setAttribute('class', 'project-desc1') ;
+        newText.setAttribute('id', 'project-description') ;
+        newText.setAttribute('style', 'margin-bottom:2px') ;
+        $("#theProject").append(newText) ;
+        page.find('.project-desc1').append(item.project.description[0]);
+
+        var newText2 = document.createElement('h5');
+        newText2.setAttribute('class', 'project-desc2') ;
+        newText2.setAttribute('id', 'project-description') ;
+        newText2.setAttribute('style', 'margin-bottom:2px') ;
+        $("#theProject").append(newText2) ;
+        page.find('.project-desc2').append(item.project.description[1]);
+
+        var newText3 = document.createElement('h5');
+        newText3.setAttribute('class', 'project-desc3') ;
+        newText3.setAttribute('id', 'project-description') ;
+        $("#theProject").append(newText3) ;
+        page.find('.project-desc3').append(item.project.description[2]);
+
+        var newText4 = document.createElement('h5');
+        newText4.setAttribute('class', 'project-desc4') ;
+        newText4.setAttribute('id', 'project-description') ;
+        $("#theProject").append(newText4) ;
+        page.find('.project-desc4').append(item.project.description[3] + "<br>" +"<br>");
+
+        var newText5 = document.createElement('h5');
+        newText5.setAttribute('class', 'project-desc5') ;
+        newText5.setAttribute('id', 'project-description') ;
+        $("#theProject").append(newText5) ;
+        page.find('.project-desc5').append(item.project.description[4] + "<br>" +"<br>");
+
+        if(item.project.image[0] != "") {
+          for(var i = 0 ; i < item.project.image.length ;i++) {
+            var newImage = document.createElement('img');
+            newImage.id = "projectImages" ;
+            console.log(newImage.id) ;
+            newImage.setAttribute('src',"project/"+ item.id + "/" + item.project.image[i] );
+            newImage.setAttribute('style', "width:50vw; margin-bottom:30px;")
+            $( "#theProject" ).append(newImage) ;
+          }
+        }
+
+        if(item.project.hasOwnProperty('video')) {
+          for(var i = 0 ; i < item.project.video.length ; i++) {
+            var newVideo = document.createElement('iframe') ;
+            newVideo.setAttribute('src', item.project.video[i]) ;
+            newVideo.setAttribute('allowFullScreen', 'true')
+            $('#theProject').append(newVideo) ;
+          }
+        }
+
+
+        for(var i = 0 ; i < item.project['thesis-category'].length ; i++) {
+           page.find('.thesis-category').append(item.project['thesis-category'][i] + "<br>") ;
+        }
+
+        for(var i = 0 ; i < item.project['tags'].length ; i++) {
+          page.find('.myTags').append(item.project['tags'][i] + "<br>") ;
+        }
+        page.find('.studio1').append(item.project['studio-professor-1']+"<br>") ;
+        page.find('.studio1').append(item.project['writing-professor-1']) ;
+        if(item.project.hasOwnProperty('other-professor-1')) {
+          console.log("ran") ;
+          page.find('.studio1').append("<br>" + item.project['other-professor-1']) ;
+        }
+        page.find('.studio2').append(item.project['studio-professor-2']+"<br>") ;
+        page.find('.studio2').append(item.project['writing-professor-2']) ;
+        if(item.project.hasOwnProperty('other-professor-2')) {
+          page.find('.studio2').append("<br>" + item.project['other-professor-2']) ;
+        }
+
+        container.find('h4').text(item.project.blurb);
+        container.find('p').text(item.project.description);
+      } else if(item.id == 65 && item.id == index) {
         //paolo's page
         page.find('.page-name').text(item.name);
         page.find('.page-image').find('img').attr('src',"portraits/"+item['profile-pic']);
@@ -457,7 +619,7 @@ function renderSingleProjectPage(index, data){
         newImage7.id = "projectImages" ;
         newImage7.setAttribute('src',"project/"+ item.id + "/" + item.project.image[6] );
         newImage7.setAttribute('style', "width:50vw; margin-bottom:30px;")
-        $( "#theProject" ).append(newImage5) ;
+        $( "#theProject" ).append(newImage7) ;
         var newImage8 = document.createElement('img');
         newImage8.id = "projectImages" ;
         newImage8.setAttribute('src',"project/"+ item.id + "/" + item.project.image[7] );
